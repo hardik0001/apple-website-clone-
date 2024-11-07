b02775d4f93273146f8b989ef1ce8da21901c345
Hardik Bharti <hardik.bharti@herovired.com>
2024-11-07 15:54:15 +0100

    setup all the data for the project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import "./index.css"
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlight from "./components/Highlight"
+
+function App() {
+
+
+
+  return (
+    <>
+
+      <main className="bg-black">
+        <Navbar />
+
+        <Hero />
+        <Highlight />
+
+
+      </main>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import react from 'react'
+
+const Hero = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Hero
